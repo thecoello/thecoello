42a2163d93811dae19c4b733fa4663d50a409d63
thecoello <61286111+thecoello@users.noreply.github.com>
2021-03-05 14:53:02 +0100

    Add files via upload

--- js/js-pure/menu.js
+++ js/js-pure/menu.js
@@ -1,48 +1,46 @@
-function menuOpen() {
-  const Ease = { duration: 0.2, ease: "power2.out" };
+const Ease = { duration: 0.2, ease: "power2.out" };
 
-  document.querySelector(".menubtn").onclick = function () {
-    document.querySelector("body").style.overflow = "hidden";
+document.querySelector(".menubtn").onclick = function () {
+  document.querySelector("body").style.overflow = "hidden";
 
-    gsap.to(".menubtn", {
-      duration: 0.1,
+  gsap.to(".menubtn", {
+    duration: 0.1,
+    ease: Ease,
+    top: "-50px",
+    display: "none",
+  });
+
+  gsap.to(".navigation-list", {
+    backgroundColor: "black",
+  });
+
+  setTimeout(function () {
+    gsap.from(".navigation-list", {
+      scale: 0.95,
+      autoAlpha: 0,
       ease: Ease,
-      top: "-50px",
-      display: "none",
     });
+  }, 300);
 
-    gsap.to(".navigation-list",{
-      backgroundColor: "black"
-    })
+  setTimeout(function () {
+    gsap.to(".navigation-list", {
+      display: "flex",
+      scale: 1,
+      autoAlpha: 1,
+      ease: Ease,
+    });
+  }, 300);
 
-    setTimeout(function () {
-      gsap.from(".navigation-list", {
-        scale: 0.95,
-        autoAlpha: 0,
-        ease: Ease,
-      });
-    }, 300);
-
-    setTimeout(function () {
-      gsap.to(".navigation-list", {
-        display: "flex",
-        scale: 1,
-        autoAlpha: 1,
-        ease: Ease,
-      });
-    }, 300);
-
-    setTimeout(function () {
-      gsap.to(".menuclose", {
-        duration: 0.1,
-        top: "35px",
-        display: "initial",
-        filter: "invert(100%",
-        ease: Ease,
-      });
-    }, 600);
-  };
-}
+  setTimeout(function () {
+    gsap.to(".menuclose", {
+      duration: 0.1,
+      top: "35px",
+      display: "initial",
+      filter: "invert(100%",
+      ease: Ease,
+    });
+  }, 600);
+};
 
 function menuClose() {
   const Ease = { duration: 0.2, ease: "power2.out" };
@@ -84,24 +82,20 @@ function menuClose() {
   }, 600);
 }
 
-function menuCloseBtn() {
-  document.querySelector(".menuclose").onclick = function () {
-    menuClose();
-  };
-}
+document.querySelector(".menuclose").onclick = function () {
+  menuClose();
+};
 
-function menuCloseClickItem() {
-  const menuItem = document.querySelectorAll(
-    ".header-container .navigation-list"
-  );
+const menuItem = document.querySelectorAll(
+  ".header-container .navigation-list"
+);
 
-  for (let i = 0; i < menuItem.length; i++) {
-    const menu = menuItem[i].children;
+for (let i = 0; i < menuItem.length; i++) {
+  const menu = menuItem[i].children;
 
-    for (const item of menu) {
-      item.children[0].onclick = function () {
-        menuClose();
-      };
-    }
+  for (const item of menu) {
+    item.children[0].onclick = function () {
+      menuClose();
+    };
   }
 }
